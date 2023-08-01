3921e0ff747dd6e6e6b4aaf06696cb9c4834df5f
ofisch <onni.fischer@gmail.com>
2023-08-01 13:11:56 +0300

    tulostus localStoragesta

--- main.js
+++ main.js
@@ -3,7 +3,48 @@ window.addEventListener('load', () => {
     const input = document.querySelector('#new-task-input');
     const listElement = document.querySelector('#tasks');
 
-    const elements = [];
+    const elements = JSON.parse(localStorage.getItem('elements'));
+
+
+    // tulostetaan localStoragessa olevat elementit
+    for (i of elements) {
+
+        // task-elementin rakentaminen
+        const task = document.createElement('div');
+        task.classList.add('task');
+
+        const taskContent = document.createElement('div');
+        taskContent.classList.add('content');
+
+        task.appendChild(taskContent);
+
+        const taskInput = document.createElement('input');
+        taskInput.classList.add('text');
+        taskInput.type = 'text';
+        taskInput.value = i;
+        taskInput.setAttribute('readonly', 'readonly');
+
+        taskContent.appendChild(taskInput);
+        
+        const taskActions = document.createElement('div');
+        taskActions.classList.add('actions');
+
+        const taskEdit = document.createElement('button');
+        taskEdit.classList.add('edit');
+        taskEdit.innerHTML = 'Muokkaa';
+
+        const taskDelete = document.createElement('button');
+        taskDelete.classList.add('delete');
+        taskDelete.innerHTML = 'Poista';
+
+        taskActions.appendChild(taskEdit);
+        taskActions.appendChild(taskDelete);
+
+        task.appendChild(taskActions);
+
+        listElement.appendChild(task);
+
+    }
 
     form.addEventListener('submit', (e) => {
         e.preventDefault();
@@ -71,7 +112,17 @@ window.addEventListener('load', () => {
         })
 
         taskElementDelete.addEventListener('click', () => {
-            listElement.removeChild(taskElement);        
+            // poistetaan elementtiListasta
+            listElement.removeChild(taskElement);
+            // poistetaan taulukosta
+            for (i of elements) {
+                if (i == taskElement.firstChild.firstChild.value) {
+                    console.log('poistettava arvo: ', taskElement.firstChild.firstChild.value);
+                    elements.splice(elements.indexOf(i), 1);
+                    // päivitetään localStorage
+                    localStorage.setItem('elements', JSON.stringify(elements));
+                }
+            }        
         })
 
     })
