3953e738579410c50a01198aefb681ebe2e61a44
ofisch <onni.fischer@gmail.com>
2023-08-01 13:53:02 +0300

    poistaminen toimii

--- main.js
+++ main.js
@@ -1,41 +1,45 @@
-window.addEventListener('load', () => {
-    const form = document.querySelector('#new-task-form');
-    const input = document.querySelector('#new-task-input');
-    const listElement = document.querySelector('#tasks');
+window.addEventListener("load", () => {
+  const form = document.querySelector("#new-task-form");
+  const input = document.querySelector("#new-task-input");
+  const listElement = document.querySelector("#tasks");
 
-    const elements = JSON.parse(localStorage.getItem('elements'));
+  let elementsToSave = [];
+  const elements = JSON.parse(localStorage.getItem("elements"));
 
-
-    // tulostetaan localStoragessa olevat elementit
-    for (i of elements) {
-
+  // tulostetaan localStoragessa olevat elementit
+  const renderElements = () => {
+    listElement.innerHTML = "";
+    elementsToSave = [];
+    if (elements != null) {
+      console.log("noni tulostetaas");
+      for (i of elements) {
         // task-elementin rakentaminen
-        const task = document.createElement('div');
-        task.classList.add('task');
+        const task = document.createElement("div");
+        task.classList.add("task");
 
-        const taskContent = document.createElement('div');
-        taskContent.classList.add('content');
+        const taskContent = document.createElement("div");
+        taskContent.classList.add("content");
 
         task.appendChild(taskContent);
 
-        const taskInput = document.createElement('input');
-        taskInput.classList.add('text');
-        taskInput.type = 'text';
+        const taskInput = document.createElement("input");
+        taskInput.classList.add("text");
+        taskInput.type = "text";
         taskInput.value = i;
-        taskInput.setAttribute('readonly', 'readonly');
+        taskInput.setAttribute("readonly", "readonly");
 
         taskContent.appendChild(taskInput);
-        
-        const taskActions = document.createElement('div');
-        taskActions.classList.add('actions');
 
-        const taskEdit = document.createElement('button');
-        taskEdit.classList.add('edit');
-        taskEdit.innerHTML = 'Muokkaa';
+        const taskActions = document.createElement("div");
+        taskActions.classList.add("actions");
 
-        const taskDelete = document.createElement('button');
-        taskDelete.classList.add('delete');
-        taskDelete.innerHTML = 'Poista';
+        const taskEdit = document.createElement("button");
+        taskEdit.classList.add("edit");
+        taskEdit.innerHTML = "Muokkaa";
+
+        const taskDelete = document.createElement("button");
+        taskDelete.classList.add("delete");
+        taskDelete.innerHTML = "Poista";
 
         taskActions.appendChild(taskEdit);
         taskActions.appendChild(taskDelete);
@@ -44,62 +48,57 @@ window.addEventListener('load', () => {
 
         listElement.appendChild(task);
 
+        elementsToSave.push(i);
+
+        taskDelete.addEventListener("click", () => {
+          for (i of elements) {
+            if (i == task.firstChild.firstChild.value) {
+              console.log(
+                "poistettava arvo: ",
+                task.firstChild.firstChild.value
+              );
+              elements.splice(elements.indexOf(i), 1);
+              // päivitetään localStorage
+              localStorage.setItem("elements", JSON.stringify(elements));
+              location.reload();
+            }
+          }
+        });
+      }
+      console.log("elementsToSave: ", elementsToSave);
+    }
+  };
+
+  renderElements();
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const task = input.value;
+
+    if (!task) {
+      alert("täytä tekstikenttä");
+      return;
     }
 
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
+    // elementin tallentamineen elements-taulukkoon
+    elementsToSave.push(task);
+    console.log("uusin elementti joka tallennetaan: ", elementsToSave);
+    // eka localStorage tyhjäks
+    localStorage.clear();
+    // päivitetyn elements-taulukon tallentaminen localStorageen
+    localStorage.setItem("elements", JSON.stringify(elementsToSave));
 
-        const task = input.value;
+    input.value = "";
 
-        if (!task) {
-            alert("täytä tekstikenttä");
-            return;
-        } 
+    /*
+        // tulostetaan elementit uudestaan (päivitettynä)
+        renderElements();
+        */
 
-        const taskElement = document.createElement('div');
-        taskElement.classList.add('task');
+    location.reload();
 
-        const taskElementContent = document.createElement('div');
-        taskElementContent.classList.add('content');
-
-        taskElement.appendChild(taskElementContent);
-
-        const taskElementInput = document.createElement('input');
-        taskElementInput.classList.add('text');
-        taskElementInput.type = 'text';
-        taskElementInput.value = task;
-        taskElementInput.setAttribute('readonly', 'readonly');
-
-        taskElementContent.appendChild(taskElementInput);
-        console.log('taskElementInput: ', taskElementInput.value);
-
-
-        // elementin tallentamineen elements-taulukkoon 
-        elements.push(taskElementInput.value);
-        // elements-taulukon tallentaminen localStorageen
-        localStorage.setItem('elements', JSON.stringify(elements));
-
-
-        const taskElementActions = document.createElement('div');
-        taskElementActions.classList.add('actions');
-
-        const taskElementEdit = document.createElement('button');
-        taskElementEdit.classList.add('edit');
-        taskElementEdit.innerHTML = 'Muokkaa';
-
-        const taskElementDelete = document.createElement('button');
-        taskElementDelete.classList.add('delete');
-        taskElementDelete.innerHTML = 'Poista';
-
-        taskElementActions.appendChild(taskElementEdit);
-        taskElementActions.appendChild(taskElementDelete);
-
-        taskElement.appendChild(taskElementActions);
-
-        listElement.appendChild(taskElement);
-
-        input.value = '';
-
+    /*
         taskElementEdit.addEventListener('click', () => {
             if (taskElementEdit.innerText.toLowerCase() == 'muokkaa') {
                 taskElementInput.removeAttribute('readonly');
@@ -124,6 +123,6 @@ window.addEventListener('load', () => {
                 }
             }        
         })
-
-    })
-})
+        */
+  });
+});
